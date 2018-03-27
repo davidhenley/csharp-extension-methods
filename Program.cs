7c3b85b73657fe2305449619f3aa5706680fb05e
David Henley <henleydr@gmail.com>
2018-03-27 16:31:06 -0500

    Setup of extension method

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string post = "This is supposed to be a very long blog post blah blah blah.";
+
+                // You cannot inherit from string because it is sealed
+                // You must create an extension on that string class
+
+                var shortenedPost = post.Shorten(5);
+
+                Console.WriteLine(shortenedPost);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        // To create an extension method, use 'this' in front of the first argument
+        public static string Shorten(this string str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException(nameof(numberOfWords), "numberOfWords must be a positive number.");
+
+            var wordsArray = str.Split(' ');
+
+            if (wordsArray.Length <= numberOfWords)
+            {
+                return str;
+            }
+
+            return String.Join(" ", wordsArray.Take(numberOfWords)) + "...";
+        }
+    }
+}
